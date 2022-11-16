ae0ffac01d0630248231ce41c817b6234c95b22b
ancxanas <anas.corona.github@gmail.com>
2022-11-16 22:30:43 +0530

    Changed the course and its parts into a single Javascript object

--- part1/courseinfo/courseinfo/src/App.js
+++ part1/courseinfo/courseinfo/src/App.js
@@ -2,7 +2,7 @@ const Header = (props) => {
   console.log(props);
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 };
@@ -22,7 +22,6 @@ const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <Part />
       <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
       <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
@@ -44,18 +43,20 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of component', exercises: 14 },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };

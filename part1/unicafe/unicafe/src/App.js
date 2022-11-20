4401009535806ce4d2096caa20a745719e26d411
ancxanas <anas.corona.github@gmail.com>
2022-11-20 16:40:20 +0530

    completed unicafe step2

--- part1/unicafe/unicafe/src/App.js
+++ part1/unicafe/unicafe/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Buttons = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodPlusOne = () => setGood(good + 1);
+  const neutralPlusOne = () => setNeutral(neutral + 1);
+  const badPlusOne = () => setBad(bad + 1);
+
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const percentage = (good / all) * 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header text="give feedback" />
+      <Buttons onClick={goodPlusOne} text="good" />
+      <Buttons onClick={neutralPlusOne} text="neutral" />
+      <Buttons onClick={badPlusOne} text="bad" />
+      <Header text="statistics" />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>percentage {percentage}</p>
+    </>
   );
-}
+};
 
 export default App;

48fdb622664cb9fdd3ce7c422c0eb45b0bf00702
ancxanas <anas.corona.github@gmail.com>
2022-11-24 14:10:29 +0530

    Declared the course component in a separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,4 @@
-const Course = ({ name, parts }) => {
-  const total = parts.reduce((s, p) => s + p.exercises, 0);
-
-  return (
-    <div>
-      <Header name={name} />
-      <Content parts={parts} />
-      <Total sum={total} />
-    </div>
-  );
-};
-
-const Header = ({ name }) => <h2>{name}</h2>;
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} name={part.name} exercises={part.exercises} />
-    ))}
-  </>
-);
-
-const Total = ({ sum }) => (
-  <p style={{ fontWeight: 'bold' }}>total of {sum} exercises</p>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [

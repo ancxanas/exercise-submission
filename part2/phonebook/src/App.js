a72e7e2654fc223dee2d1fd5b3e64c8dd90ca783
ancxanas <anas.corona.github@gmail.com>
2022-11-26 19:15:27 +0530

    Added the functionality to add phone numbers to phone book

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 
-const Number = ({ name }) => <li>{name}</li>;
+const Number = ({ name, number }) => (
+  <li>
+    {name} {number}
+  </li>
+);
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addName = (e) => {
     e.preventDefault();
@@ -13,14 +20,20 @@ const App = () => {
     if (nameCheck) return alert(`${newName} is already added to phonebook`);
     const nameObject = {
       name: newName,
+      number: newNumber,
     };
 
     setPersons(persons.concat(nameObject));
     setNewName('');
+    setNewNumber('');
   };
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -31,16 +44,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <Number key={person.name} name={person.name} />
+          <Number key={person.name} name={person.name} number={person.number} />
         ))}
       </ul>
-      <div>debug: {newName}</div>
     </div>
   );
 };

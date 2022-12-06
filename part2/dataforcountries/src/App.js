c868be76a6953e63f6bb6616d45ea4a1935350f2
ancxanas <anas.corona.github@gmail.com>
2022-12-06 11:58:53 +0530

    Created dataforcountries app

---
+++ part2/dataforcountries/src/App.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  return (
+    <>
+      find countries <input />
+    </>
+  );
+};
+
+export default App;

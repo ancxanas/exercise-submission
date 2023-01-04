612351de61a735cc2f926ff5f0ef487a87231fe2
ancxanas <anas.corona.github@gmail.com>
2023-01-05 00:05:10 +0530

    Added css to keep the button in the same line

--- part2/phonebook/src/components/Number.js
+++ part2/phonebook/src/components/Number.js
@@ -1,10 +1,10 @@
 const Number = ({ name, number, onClick }) => (
-  <>
+  <div style={{ display: 'flex', flexDirection: 'row' }}>
     <div>
       {name} {number}
     </div>
     <button onClick={onClick}>delete</button>
-  </>
+  </div>
 );
 
 export default Number;

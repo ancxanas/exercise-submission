7591fdac07ec014588b1412aa2482a793c1f0f7f
ancxanas <anas.corona.github@gmail.com>
2023-01-19 14:57:56 +0530

    Changed baseUrl

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://silent-water-1018.fly.dev/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);

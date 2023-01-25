bcdcebb609336c56c11b9ffa5c877ed12b364596
ancxanas <anas.corona.github@gmail.com>
2023-01-25 22:18:10 +0530

    Configured ESlint

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,10 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 
-morgan.token('body', (request, response) => JSON.stringify(request.body));
+morgan.token('body', (request) => JSON.stringify(request.body));
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')

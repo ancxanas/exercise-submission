16530177130915cf1111142bb2fa27a0c86c0e5a
ancxanas <anas.corona.github@gmail.com>
2023-02-05 13:04:45 +0530

    Implemented functionality for deleting and finding single blog post resource Implemented test for deleting functionality

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,9 +1,24 @@
 const blogsRouter = require('express').Router();
+const { isValidObjectId } = require('mongoose');
+const { response } = require('../app');
 const Blog = require('../models/blog');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
+});
+
+blogsRouter.get('/:id', async (request, response) => {
+  if (!isValidObjectId(request.params.id)) {
+    return response.status(400).end();
+  }
+
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
 });
 
 blogsRouter.post('/', async (request, response) => {
@@ -18,6 +33,11 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = blogsRouter;

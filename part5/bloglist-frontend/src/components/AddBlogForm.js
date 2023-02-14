335f631c64abe0e6e545bd1416515a2e9ca11e32
ancxanas <anas.corona.github@gmail.com>
2023-02-14 21:33:10 +0530

    Refactored AddBlogForm to its own module

---
+++ part5/bloglist-frontend/src/components/AddBlogForm.js
@@ -0,0 +1,45 @@
+const AddBlogForm = ({
+  addBlog,
+  title,
+  author,
+  url,
+  setTitle,
+  setAuthor,
+  setUrl,
+}) => (
+  <div>
+    <h2>create new</h2>
+    <form onSubmit={addBlog}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          type="text"
+          value={author}
+          name="author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          type="url"
+          value={url}
+          name="url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  </div>
+);
+
+export default AddBlogForm;

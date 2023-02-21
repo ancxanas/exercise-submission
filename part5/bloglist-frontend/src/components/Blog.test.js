926ae9fbc1b22433571f98a47d7c2d333352dbdc
ancxanas <anas.corona.github@gmail.com>
2023-02-21 21:49:11 +0530

    Wrote a test to check if the like button is clicked twice the the event handler the component received as props is called twice

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,52 +4,67 @@ import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders only title and author of the blog by default', () => {
-  const blog = {
-    title: 'Titanic',
-    author: 'James Cameroon',
-    likes: 0,
-    url: 'https://titanic.com/',
-  }
+describe('<Note />', () => {
+  let blog, userObject
 
-  render(<Blog blog={blog} />)
+  beforeEach(() => {
+    blog = {
+      title: 'Titanic',
+      author: 'James Cameroon',
+      likes: 0,
+      url: 'https://titanic.com/',
+      user: {
+        name: 'James',
+        username: 'james',
+      },
+    }
 
-  const titleAndAuthorElement = screen.getAllByText('Titanic James Cameroon')
-  const likesElement = screen.queryByText('likes 0', { exact: false })
-  const urlElement = screen.queryByText('https://titanic.com/', {
-    exact: false,
-  })
-
-  expect(titleAndAuthorElement).toBeDefined()
-  expect(likesElement).toBeNull()
-  expect(urlElement).toBeNull()
-})
-
-test('renders likes and url of the blog when view is pressed', async () => {
-  const blog = {
-    title: 'Titanic',
-    author: 'James Cameroon',
-    likes: 0,
-    url: 'https://titanic.com/',
-    user: {
+    userObject = {
       name: 'James',
       username: 'james',
-    },
-  }
+    }
+  })
 
-  const userObject = {
-    username: 'james',
-  }
+  test('renders only title and author of the blog by default', () => {
+    render(<Blog blog={blog} />)
 
-  render(<Blog blog={blog} user={userObject} />)
+    const titleAndAuthorElement = screen.getAllByText('Titanic James Cameroon')
+    const likesElement = screen.queryByText('likes 0', { exact: false })
+    const urlElement = screen.queryByText('https://titanic.com/', {
+      exact: false,
+    })
 
-  const user = userEvent.setup()
-  const button = screen.getByText('view')
-  await user.click(button)
+    expect(titleAndAuthorElement).toBeDefined()
+    expect(likesElement).toBeNull()
+    expect(urlElement).toBeNull()
+  })
 
-  const likeElement = screen.getByText('likes 0')
-  const urlElement = screen.getByText('https://titanic.com/')
+  test('renders likes and url of the blog when view is pressed', async () => {
+    render(<Blog blog={blog} user={userObject} />)
 
-  expect(likeElement).toBeDefined()
-  expect(urlElement).toBeDefined()
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const likeElement = screen.getByText('likes 0')
+    const urlElement = screen.getByText('https://titanic.com/')
+
+    expect(likeElement).toBeDefined()
+    expect(urlElement).toBeDefined()
+  })
+
+  test('clicking like button twice triggers the event handler twice', async () => {
+    const updatedLike = jest.fn()
+
+    render(<Blog blog={blog} user={userObject} updatedLike={updatedLike} />)
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.dblClick(likeButton)
+
+    expect(updatedLike.mock.calls).toHaveLength(2)
+  })
 })

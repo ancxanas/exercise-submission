b6c8ff88f744fbdbda68dcc9f889ac5a64298c3e
ancxanas <anas.corona.github@gmail.com>
2023-02-26 14:06:36 +0530

    Wrote tests for logging in

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', () => {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'Muhammed Anas',
+      username: 'anas',
+      password: 'paika',
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
     cy.visit('')
   })
 
@@ -8,5 +14,28 @@ describe('Blog app', () => {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('anas')
+      cy.get('#password').type('paika')
+      cy.get('#login-button').click()
+
+      cy.contains('Muhammed Anas logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('anas')
+      cy.get('#password').type('kallangadi')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Muhammed Anas logged in')
+    })
   })
 })

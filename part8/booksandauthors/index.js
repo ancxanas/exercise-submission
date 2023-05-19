c369cb2b5db4b43daac7ea487dce2be6f118735c
ancxanas <anas.corona.github@gmail.com>
2023-05-19 20:25:13 +0530

    Added connection to MongoDB

--- part8/booksandauthors/index.js
+++ part8/booksandauthors/index.js
@@ -2,6 +2,27 @@ const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const Book = require('./models/book')
+const Author = require('./models/author')
+
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
 let authors = [
   {
     name: 'Robert Martin',
@@ -104,7 +125,7 @@ const typeDefs = `
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     genres: [String!]!
     id: ID!
   }

2ec471648c6ae1fd2efcafccfd6d7b5f75ebc9a2
ancxanas <anas.corona.github@gmail.com>
2023-07-09 14:17:13 +0530

    Added services to fetch diagnoses data

--- part9/patientor/src/routes/diagnoses.ts
+++ part9/patientor/src/routes/diagnoses.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import diagnosesService from '../services/diagnosesService';
 
 const diagnosesRouter = express.Router();
 
 diagnosesRouter.get('/', (_req, res) => {
-  res.send('Fetching all diagnoses');
+  res.send(diagnosesService.getDiagnoses());
 });
 
 export default diagnosesRouter;

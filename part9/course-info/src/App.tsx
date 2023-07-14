da2bfbfbef6e385c6b27fe0effcca36c1f5ded6c
ancxanas <anas.corona.github@gmail.com>
2023-07-14 20:09:42 +0530

    Refactored the code to several components and used type declarations

---
+++ part9/course-info/src/App.tsx
@@ -0,0 +1,51 @@
+interface CourseParts {
+  name: string;
+  exerciseCount: number;
+}
+
+const Header = ({ name }: { name: string }) => <h1>{name}</h1>;
+
+const Content = ({ courseParts }: { courseParts: CourseParts[] }) => (
+  <>
+    {courseParts.map((course) => (
+      <p key={course.name}>
+        {course.name} {course.exerciseCount}
+      </p>
+    ))}
+  </>
+);
+
+const Total = ({ courseParts }: { courseParts: CourseParts[] }) => (
+  <p>
+    Number of exercises
+    {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+  </p>
+);
+
+const App = () => {
+  const courseName = 'Half Stack application development';
+  const courseParts = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
